08f379cdeb557c769fe977b60b0c0b256929e787
houtaruoreki <l.maghlaperidze@gmail.com>
2024-04-08 11:24:48 +0400

    modified serializers added gitignore

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -1,3 +1,5 @@
+import re
+
 from rest_framework import serializers
 
 from .models import User
@@ -8,7 +10,7 @@ class UserLoginSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['email', 'password']
-    
+
 
 class UserRegisterSerializer(serializers.ModelSerializer):
     confirm_password = serializers.CharField(write_only=True)
@@ -18,8 +20,22 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         fields = ['name', 'number', 'email', 'password', 'confirm_password']
 
     def validate(self, data):
-        if data.get('password') != data.get('confirm_password'):
+        password = data.get('password')
+        if password != data.get('confirm_password'):
             raise serializers.ValidationError("The passwords do not match.")
+
+        if len(password) < 8:
+            raise serializers.ValidationError("The password is less than 8 characters.")
+
+        if not re.search(r'[!@#$%^&*()_+{}|:"<>?~\-\[\];\',./]', password):
+            raise serializers.ValidationError("The password must contain at least one special character.")
+
+        if not re.search(r'\d', password):
+            raise serializers.ValidationError("The password must contain at least one digit.")
+
+        if not re.search(r'[A-Z]', password):
+            raise serializers.ValidationError("The password must contain at least one uppercase letter.")
+
         return data
 
     def create(self, validated_data):
