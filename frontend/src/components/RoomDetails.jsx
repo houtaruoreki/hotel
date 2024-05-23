36aa914cf87a8f3246ebc62be468c25c64420217
houtaruoreki <l.maghlaperidze@gmail.com>
2024-05-24 01:58:01 +0400

    Update RoomDetails

--- frontend/src/components/RoomDetails.jsx
+++ frontend/src/components/RoomDetails.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Image from "/Images/image 8.png";
 
 export default function RoomDetails() {
   const [roomDetails, setRoomDetails] = useState(null);
@@ -20,29 +21,28 @@ export default function RoomDetails() {
   return (
     <div className="bg-white px-16 pb-5">
       <div className="text-center">
-        <h2 className="text-white text-2xl font-bold pt-[96px]">
+        <h2 className="text-mwvane text-2xl font-bold pt-[96px]">
           ოთახის დეტალები
         </h2>
-        <p className="text-[#D9D9D9] mt-14 text-center text-xl font-light">
+        <p className="text-[#2D3648] mt-14 text-center text-xl font-light">
           დატკბით დასვენებით და სტუმართმოყვარეობით ჩვენს ელეგანტურ ოთახებში.
           ლამაზი ხედებითა და პრემიუმ კეთილმოწყობით, თქვენი დასვენება იქნება
           არაჩვეულებრივი გამოცდილება
         </p>
       </div>
       <div className="flex mt-[100px] gap-[85px]">
-        <div className="w-[648px] h-[503px] bg-gray-500">1</div>
+        <div className="w-[648px] h-[503px] bg-gray-500">
+          <img src={Image} className=""/>
+        </div>
         <div>
-          <h2 className="text-xl text-white font-bold mt-5">{roomDetails.number}</h2>
-          <p className="text-l text-[#D9D9D9] font-normal mt-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. At purus
-            tellus arcu sit nibh consectetur.
+          <h2 className="text-xl text-black font-bold mt-5">ოთახის ნომერი</h2>
+          <p className="text-l text-[#2D3648] font-normal mt-4">
+            {roomDetails.number}
           </p>
-          <h2 className="text-xl text-white font-bold mt-5">აღწერა</h2>
-          <p className="text-l text-[#D9D9D9] font-normal mt-4">{roomDetails.description}</p>
-          <h2 className="text-xl text-white font-bold mt-5">სტატუსი </h2>
-          <p className="text-l text-[#D9D9D9] font-normal mt-4">{roomDetails.status ? "დაკავაებულია" : "თავისუფალია"}</p>
-          <h2 className="text-xl text-white font-bold mt-5">სტუმრების რაოდენობა</h2>
-          <p className="text-l text-[#D9D9D9] font-normal mt-4">ერთი</p>
+          <h2 className="text-xl text-black font-bold mt-5">აღწერა</h2>
+          <p className="text-l text-[#2D3648] font-normal mt-4">{roomDetails.description}</p>
+          <h2 className="text-xl text-black font-bold mt-5">სტატუსი </h2>
+          <p className="text-l text-[#2D3648] font-normal mt-4">{roomDetails.status ? "დაკავაებულია" : "თავისუფალია"}</p>
         </div>
       </div>
     </div>
